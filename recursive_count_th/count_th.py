4d507ccba08c648c5b3d6752fbc35f3e47d63821
Jessica <duellje@oregonstate.edu>
2020-09-18 11:56:13 -0400

    completed count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,7 +7,7 @@ def count_th(word):
     if len(word) == 0 or len(word) == 1:
         return 0
     if word[0] == 't' and word[1] == 'h':
-        return 1
+        return 1 + count_th(word[2:])
     else:
         return count_th(word[1:])
     
--- recursive_count_th/test_count_th.py
+++ recursive_count_th/test_count_th.py
@@ -12,25 +12,25 @@ class Test(unittest.TestCase):
         count = count_th(self.word)
         self.assertEqual(0, count)
 
-    # def test_count_th_single(self):
-    #     self.word = "abcthxyz"
-    #     count = count_th(self.word)
-    #     self.assertEqual(1, count)
+    def test_count_th_single(self):
+        self.word = "abcthxyz"
+        count = count_th(self.word)
+        self.assertEqual(1, count)
     
-    # def test_count_th_multiple(self):
-    #     self.word = "abcthefthghith"
-    #     count = count_th(self.word)
-    #     self.assertEqual(3, count)
+    def test_count_th_multiple(self):
+        self.word = "abcthefthghith"
+        count = count_th(self.word)
+        self.assertEqual(3, count)
 
-    # def test_count_backwards(self):
-    #     self.word = "thhtthht"
-    #     count = count_th(self.word)
-    #     self.assertEqual(2, count)
+    def test_count_backwards(self):
+        self.word = "thhtthht"
+        count = count_th(self.word)
+        self.assertEqual(2, count)
 
-    # def test_count_th_mixedcase(self):
-    #     self.word = "THtHThth"
-    #     count = count_th(self.word)
-    #     self.assertEqual(1, count)
+    def test_count_th_mixedcase(self):
+        self.word = "THtHThth"
+        count = count_th(self.word)
+        self.assertEqual(1, count)
 
     
 if __name__ == '__main__':
