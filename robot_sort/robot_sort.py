466d933b6887ddcdf8f7aef37441cce2aa9c1090
Jessica <duellje@oregonstate.edu>
2020-09-18 14:01:02 -0400

    reached MVP

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,7 +96,7 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        
+        #first pass
         while self.can_move_right() and not self.light_is_on():
             #move to the right and pick up item
             self.move_right()
@@ -131,6 +131,29 @@ class SortingRobot:
             if self.can_move_left() is False:
                 self.swap_item()
                 self.set_light_off()
+        
+        # while self.can_move_right() and not self.light_is_on():
+        #     #move to the right and pick up item
+        #     self.move_right()
+        #     self.swap_item()
+        #     #turn light on to indicate holding item
+        #     self.set_light_on()
+        #     #items on left and holding item
+            
+        #     while self.can_move_left() and self.light_is_on():
+        #         self.move_left()
+            
+        #     if self.compare_item() == -1:
+        #         self.swap_item()
+            
+        #     while self.compare_item():
+        #         self.move_right()
+        #         if self.compare_item() == -1:
+        #             self.swap_item()
+        #     self.swap_item()
+        #     self.set_light_off()
+                
+            
                     
 if __name__ == "__main__":
     # Test our your implementation from the command line
@@ -138,6 +161,7 @@ if __name__ == "__main__":
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
     
+    # l = [10, 11, 2, 12, 13]
 
     robot = SortingRobot(l)
 
